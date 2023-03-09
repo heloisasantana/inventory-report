ac71bea63d66ddee78ff2f61ca74b7f29e39c8dd
heloisasantana <heloisasantana.silva@hotmail.com>
2023-03-09 11:46:59 -0300

    Requisito 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,27 @@
+from collections import Counter
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(data_list):
+        companies = []
+        fabrication_dates = []
+        validity_dates = []
+
+        for data_dict in data_list:
+            companies.append(data_dict["nome_da_empresa"])
+            companie_most_common = Counter(companies).most_common(1)[0][0]
+
+            fabrication_dates.append(data_dict["data_de_fabricacao"])
+
+            validity_date = data_dict["data_de_validade"]
+            validity_date_form = datetime.strptime(validity_date, "%Y-%m-%d")
+            if datetime.now() < validity_date_form:
+                validity_dates.append(data_dict["data_de_validade"])
+
+        return (
+            f"Data de fabricação mais antiga: {min(fabrication_dates)}\n"
+            f"Data de validade mais próxima: {min(validity_dates)}\n"
+            f"Empresa com mais produtos: {companie_most_common}"
+        )
