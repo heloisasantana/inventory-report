111aeba9a704e3618f51e2bd8bca1096782abf07
heloisasantana <heloisasantana.silva@hotmail.com>
2023-03-09 10:56:48 -0300

    Requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,32 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    data = {
+        "id": "1",
+        "nome_do_produto": "Nicotine Polacrilex",
+        "nome_da_empresa": "Target Corporation",
+        "data_de_fabricacao": "2021-02-18",
+        "data_de_validade": "2023-09-17",
+        "numero_de_serie": "CR25 1551 4467 2549 4402 1",
+        "instrucoes_de_armazenamento": "instrucao 1"
+    }
+
+    new_product = Product(
+        data["id"],
+        data["nome_do_produto"],
+        data["nome_da_empresa"],
+        data["data_de_fabricacao"],
+        data["data_de_validade"],
+        data["numero_de_serie"],
+        data["instrucoes_de_armazenamento"]
+    )
+
+    assert new_product.id == data["id"]
+    assert new_product.nome_do_produto == data["nome_do_produto"]
+    assert new_product.nome_da_empresa == data["nome_da_empresa"]
+    assert new_product.data_de_fabricacao == data["data_de_fabricacao"]
+    assert new_product.data_de_validade == data["data_de_validade"]
+    assert new_product.numero_de_serie == data["numero_de_serie"]
+    assert new_product.instrucoes_de_armazenamento == \
+        data["instrucoes_de_armazenamento"]
